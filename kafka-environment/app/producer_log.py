557d1c76b0c52877c164b3cff87a401985ab0795
Lev Epshtein <levep@cloudzone.io>
2025-07-23 11:06:02 +0300

    adding python container, and some code

---
+++ kafka-environment/app/producer_log.py
@@ -0,0 +1,37 @@
+from kafka import KafkaProducer
+
+from time import sleep
+import json
+# Topics/Brokers
+
+topic2 = 'kafka-tst-02'
+brokers = ["course-kafka:9092"]
+
+source_file = '/home/developer/kafka/srcFiles/srcFile.log'
+
+# First we set the producer.
+producer = KafkaProducer(
+    bootstrap_servers = brokers,
+    client_id = 'producer',
+    acks = 1,
+    compression_type = None,
+    retries = 3,
+    reconnect_backoff_ms = 50,
+    reconnect_backoff_max_ms= 1000)
+
+
+# Send the data
+with open(source_file, 'r') as f:
+    while True:
+        lines = f.readlines()  # returns list of strings
+        # print(lines)
+        if not lines:
+            sleep(1)
+            f.seek(f.tell())
+        else:
+            print(lines)
+            producer.send(topic=topic2, value=json.dumps(lines).encode('utf-8'))
+            # producer.send(topic=topic2, value=json.dumps(lines).encode('utf-8'))
+            producer.flush()
+            sleep(3)
+        # print(f.tell())
